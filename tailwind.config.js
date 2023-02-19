31207724a2f8bd60b72db083b5b48a65bce3a83a
이정중 <121495494+Gentlee-dev@users.noreply.github.com>
2023-02-19 17:17:55 +0900

    feat : 테일윈드 초기설정

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,7 @@
 /** @type {import('tailwindcss').Config} */
+const px0_10 = { ...Array.from(Array(11)).map((_, i) => `${i}px`) };
+const px0_100 = { ...Array.from(Array(101)).map((_, i) => `${i}px`) };
+const px0_500 = { ...Array.from(Array(501)).map((_, i) => `${i}px`) };
 
 module.exports = {
   // 어디에서 tailwind가 쓰일건지
@@ -7,7 +10,19 @@ module.exports = {
     "./src/app/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      borderWidth: px0_10,
+      fontSize: px0_100,
+      lineHeight: px0_100,
+      minWidth: px0_500,
+      minHeight: px0_500,
+      spacing: px0_500,
+      colors: {
+        primary: "#3888EE",
+        success: "#5FBC88",
+        error: "#E74456",
+      },
+    },
   },
   plugins: [],
 };
