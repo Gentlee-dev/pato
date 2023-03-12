9b924e3679e54fcc3014046395df2afe2567a9c6
gentlee <gentlee.dev@gmail.com>
2023-03-12 14:29:49 +0900

    faet : 검색 아톰 html재구성 및 로직 보완

--- src/Components/Atoms/searchInput.tsx
+++ src/Components/Atoms/searchInput.tsx
@@ -1,33 +1,58 @@
 "use client";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useRouter } from "next/navigation";
-
-import { Space8 } from "Components/Atoms/space";
-import SearcSVG from "/public/svg/search.svg";
+import SearchSVG from "/public/svg/search.svg";
+import XSVG from "/public/svg/x.svg";
 
 const SearchInput = () => {
   const router = useRouter();
+  const inputRef = useRef<HTMLInputElement>(null);
 
   const [keyword, setKeyword] = useState("");
 
-  const search = (e: React.FormEvent<HTMLFormElement>) => {
+  // 검색
+  const onSearch = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    setKeyword("");
+    if (keyword === "") {
+      alert("검색어를 입력 해주세요");
+      return;
+    }
     router.push(`/search/${keyword}`);
   };
+
+  // input포커스
+  const focusInput = () => {
+    inputRef?.current?.focus();
+  };
+
+  // X버튼 클릭
+  const onClickX = () => {
+    setKeyword("");
+    focusInput();
+  };
+
+  useEffect(() => {
+    focusInput();
+  }, []);
+
   return (
-    <form
-      onSubmit={search}
-      className="flex items-center h-48 p-8 rounded-xl bg-primary bg-opacity-10"
-    >
-      <SearcSVG />
-      <Space8 />
+    <form onSubmit={onSearch} className="relative flex items-center">
+      <div className="absolute left-16">
+        <SearchSVG />
+      </div>
       <input
         value={keyword}
         onChange={(e) => setKeyword(e.target.value)}
-        className="flex-[1] outline-none bg-inherit"
+        className="w-full h-48 pl-56 outline-none bg-inherit bg-primary bg-opacity-10 rounded-xl"
         type="text"
+        placeholder="상호명, 동네명 검색"
+        ref={inputRef}
       />
+      {keyword && (
+        <button type="button" onClick={onClickX} className="absolute right-16">
+          <XSVG />
+        </button>
+      )}
     </form>
   );
 };
