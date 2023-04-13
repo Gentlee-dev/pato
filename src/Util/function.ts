8f8f3ad6fc331bdcaa9822b14927b4f49ffae784
gentlee <gentlee.dev@gmail.com>
2023-04-13 23:34:47 +0900

    feat : 팔레트 유틸화

---
+++ src/Util/function.ts
@@ -0,0 +1,12 @@
+export const palete: { [index: number]: string } = {
+  1: "text-gray01",
+  2: "text-gray02",
+  3: "text-gray03",
+  4: "text-gray04",
+  5: "text-gray05",
+  6: "text-gray06",
+  7: "text-gray07",
+  8: "text-gray08",
+  9: "text-gray09",
+  10: "text-gray10",
+};
