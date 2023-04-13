10c6b48b5957dcba20a41ba6d77791fff5ed3da8
gentlee <gentlee.dev@gmail.com>
2023-04-13 23:34:24 +0900

    design : 전체패딩 제거

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => {
     <html lang="kr" className="">
       <head />
       <body className="bg-gray03">
-        <div className="max-w-3xl px-20 pt-56 bg-white m0a">{children}</div>
+        <div className="max-w-3xl pt-56 bg-white m0a">{children}</div>
       </body>
     </html>
   );
